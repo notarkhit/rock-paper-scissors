dc2f2aca62242a208d97d9076d22450d4ccf6a3f
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2022-03-01 12:26:54 +0530

    Update app.js display Result
    Generate and display result.
    
    compare user choice and computer choice  to generate the result
    
    using the rules of Rock-paper-scissors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice 
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click' ,(e)=>{
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -24,6 +26,33 @@ function generateComputerChoice() {
     if(randomNumber === 3){
         computerChoice = 'paper'
     }
-    
+
     computerChoiceDisplay.innerHTML = computerChoice 
+}
+
+function getResult() {
+
+    if(computerChoice===userChoice){
+        result = 'its a draw!'
+    }
+    if(computerChoice=='rock' && userChoice === 'paper'){
+        result = 'you win!'
+    }
+    if(computerChoice=='rock' && userChoice === 'scissors'){
+        result = 'you lost'
+    }
+    if(computerChoice=='paper' && userChoice === 'scissors'){
+        result = 'you win!'
+    }
+    if(computerChoice=='paper' && userChoice === 'rock'){
+        result = 'you lose!'
+    }
+    if(computerChoice=='scissors' && userChoice === 'rock'){
+        result = 'you win!'
+    }
+    if(computerChoice=='scissors' && userChoice === 'paper'){
+        result = 'you win!'
+    }
+
+    resultDisplay.innerHTML = result
 }
